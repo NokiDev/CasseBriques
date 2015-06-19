89aed8f3ccbc445d428f7b66a396ad0cc6a38efc
NokiDev <noki.dev@gmail.com>
2015-06-19 22:41:49 +0200

    Début d'ajout de commentaires

--- WindowSystem/WindowManager.hpp
+++ WindowSystem/WindowManager.hpp
@@ -20,22 +20,22 @@ class WindowManager
         ~WindowManager();
 
         void checkEvents();///Vérifie les évènements(fenetre, clavier, souris, etc...)
-        void updateWindow();
+        void updateWindow();///Met à jour la fenetre et tous ses éléments
         void draw(const sf::Drawable &drawable, const sf::RenderStates &states = sf::RenderStates::Default);///Dessine dans le buffer
         void display();///Affiche tous les objets stocker dans le buffer de dessin
         void clear();///Nettoie la fenêtre
 
-        void displayScreen(GuiScreen* screen);
-        void displaySubScreen();
+        void displayScreen(GuiScreen* screen);///Affiche le Gui passé en paramètre
+        void displaySubScreen();///Affiche le sous écran de la fenêtre actuelle.
 
         unsigned int getWidth();
         unsigned int getHeight();
 
     private:
-        sf::Event m_event;
-        sf::RenderWindow m_window;
-        Application* m_application;
-        GuiScreen* m_currentGuiScreen;
+        sf::Event m_event;///Event
+        sf::RenderWindow m_window;///Fenetre
+        Application* m_application;///Application
+        GuiScreen* m_currentGuiScreen;///Gui affiché
 
 
 };
